ef74935c70bb6a20ad9e2c4670c57932d84d6385
Gökhan ÖZTÜRK <GokhanOzturk@AndroidEdu.IO>
2018-07-20 22:46:19 +0300

    Changed .gitignore

---
+++ src/introduction/Hello World.kt
@@ -0,0 +1,15 @@
+package introduction
+
+// Code with ❤️
+//┌─────────────────────────────┐
+//│ Created by Gökhan ÖZTÜRK    │
+//│ ─────────────────────────── │
+//│ GokhanOzturk@AndroidEdu.IO  │            
+//│ ─────────────────────────── │
+//│ 20.07.2018 - 22:42          │
+//└─────────────────────────────┘
+
+fun main(args: Array<String>) {
+
+    print("Hello Kodluyoruz !")
+}
